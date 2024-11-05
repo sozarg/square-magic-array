9d2dfd3e3232405ba63421447906e63299c67d19
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-11-05 16:02:07 -0300

    all

--- Main.py
+++ Main.py
@@ -1,12 +1,3 @@
-# Mateo Terrile DIV 111
-
-# Actividad
-# Un cuadrado mágico es una matriz cuadrada en la que la suma de los números en cada fila, cada columna y cada diagonal principal es la misma. Esto se conoce como constante mágica del cuadrado.
-# La misma se calcula: 
-# M = n*(n2 + 1)/2
-# Formalmente, un cuadrado mágico de orden n, es una matriz cuadrada de nxn donde los números enteros del 1 al n2  están dispuestos de tal manera que la suma de los números en cada fila, cada columna y cada diagonal principal es igual a la misma constante mágica.
-# Deberán desarrollar un programa que determine si el cuadrado de valores ingresado por el usuario es o no un cuadrado mágico. Tener en cuenta todas las validaciones posibles.
-
 def calcular_constante_magica(n: int) -> int:
     return n * (n**2 + 1) // 2
 
